0ee4b9b1c513842eb9468936d94f0c706ebe8a00
suraj-phulara <165796924+suraj-phulara@users.noreply.github.com>
2024-04-08 09:46:56 +0530

    added search functionality

--- book_store/src/components/Book/Books.js
+++ book_store/src/components/Book/Books.js
@@ -8,25 +8,52 @@ Axios is a promise-based HTTP library that lets developers make requests to eith
 */
 import axios from "axios";
 import Book from "./Book";
+import { useLocation } from "react-router-dom";
 // const REACT_APP_BACKEND_ENDPOINT = process.env.REACT_APP_BACKEND_ENDPOINT;
 const URL = `/api/books`;
 
-const fetchHandler = async () => {
-  return await axios.get(URL).then((resp) => resp.data);
-  // data varialbe get all the data it take promise thats why we used then
+// const fetchHandler = async () => {
+//   return await axios.get(URL).then((resp) => resp.data);
+//   // data varialbe get all the data it take promise thats why we used then
+// };
+
+const fetchHandler = async (filterString = "") => {
+  const params = { filterString }; // Constructing an object with the filterString parameter
+  return await axios.get(URL, { params }).then((resp) => resp.data);
 };
+
 const Books = () => {
+
+  const location = useLocation();
+  
+  // Extract the search query parameter from the location object
+  const searchParams = new URLSearchParams(location.search);
+
+  const [searchQuery, setSearchQuery] = useState(searchParams.get("search") || "");
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    // console.log("sdfhjsdfkjs   :    ", searchParams.get("search") || "")
+    // const query = searchParams.get("search") || "";
+    setSearchQuery(searchParams.get("search") || "")
+    // setSearchQuery(query);
+  }, [location.search]);
+  
+  // console.log(searchQuery)
+
   // now i use eseEffect it run after browser render the components and its runs only One's if we provide any value in the array then it ryrun the array and it will redner whole page in the browser.
-  console.log("books.js url   ", URL)
+  // console.log("books.js url   ", URL)
 
   const [books, setBooks] = useState('');
   useEffect(() => {
-    fetchHandler().then((data) => setBooks(data.books));
-  }, []);
+    fetchHandler(searchQuery).then((data) => setBooks(data.books));
+  }, [searchQuery]);
   // console.log(books);
   // books () get the our two books data.
+
   return (
-    <>
+    <div>
+      
       <li>
         <ul>
           {books &&
@@ -38,7 +65,7 @@ const Books = () => {
             ))}
         </ul>
       </li>
-    </>
+    </div>
   );
 };
 export default Books;
